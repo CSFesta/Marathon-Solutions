a0bcff308edfa98260dedc5ff9ded1a252b3a80e
CSFesta <cauafesta@hotmail.com>
2025-07-02 00:06:30 -0300

    ADD: questao J

---
+++ SBC - fase zero/Fase0-22/j/README.md
@@ -0,0 +1,13 @@
+🔗 [Link do Problema](https://judge.beecrowd.com/pt/problems/view/3416)
+-
+
+
+- 🧩 **Dificuldade:** 2 / 12  
+    -
+- 🗂 **Tópicos:** [*Lógica básica*]  
+    -
+- 🧮 **Complexidade:** `O(N)`  
+    -
+- 🎯 **Desafios principais:** 
+    - 
+    - Nenhum desafio encontrado
---
+++ SBC - fase zero/Fase0-22/j/jose.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#define ll long long int
+#define pb push_back
+#define eb emplace_back
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((int)(x).size())
+
+const int MOD = 1e9 + 7;
+const double EPS = 1e-9;
+const int INF = 1e18;
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+    for(int i = 0 ; i < n; i++){
+        int x;
+        char c, lixo;
+        cin >> x >> c >> lixo;
+        if(c == '2'){
+            int total = 45;
+            total += x;
+            if(x > 45){
+                cout  << 90 << "+" << x % 45 << "\n";
+                continue;
+            }
+            cout << total << "\n";
+            continue;
+        }
+        if(x > 45){
+            cout << 45 << "+" << x % 45 << "\n";
+            continue;
+        }
+        cout << x << "\n";
+    }
+    return 0;
+}
