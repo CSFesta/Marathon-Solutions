c8ccba8e2359668bc2f11521b2c6ac9e05a50c32
CSFesta <cauafesta@hotmail.com>
2026-01-01 23:29:48 -0300

    ADD: 2012 Regional - L

---
+++ SBC - fase regional/2012/L/star.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+long long eulerPhi(long long n) {
+    long long result = n; 
+    for (long long i = 2; i * i <= n; ++i) {
+        if (n % i == 0) { 
+            while (n % i == 0) {
+                n /= i; 
+            }
+            result -= result / i; 
+        }
+    }
+    if (n > 1) {
+        result -= result / n;
+    }
+    return result;
+}
+
+int main() {
+    long long n;
+    while (cin >> n) {
+        cout << eulerPhi(n) / 2 << endl;
+    }
+    return 0;
+}
